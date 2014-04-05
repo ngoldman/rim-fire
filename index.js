d38b52f333d0bf1e4dcdefbecb4b5b28a2e50882
Nate Goldman <nnmgoldman@gmail.com>
2014-04-05 11:42:42 -0700

    add douglas county info

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var shp = require('shp');
 var server = require('./server');
 var port = process.env.PORT || 3000;
-var fireJson = shp.readFileSync('./data/USA_contiguous_and_Hawaii_24h');
+var douglasCountyTaxLots = shp.readFileSync('./data/UBE_Douglas_County_Taxlots/UBE_Douglas_County_Taxlots');
 
 function serveData() {
   this.res.writeHead(200, { 'Content-Type': 'application/json' });
-  this.res.end(JSON.stringify(fireJson));
+  this.res.end(JSON.stringify(douglasCountyTaxLots));
 }
 
 server.router.get('/data', serveData);
